bb42e37b011e61d98a81e7617026962c51a2ae2b
CocoGroudon <constantin.niclas@gmail.com>
2022-11-05 20:52:36 +0100

    entity class

--- physics.py
+++ physics.py
@@ -7,10 +7,20 @@ class Physics:
         
         self.jump = False
 
-
-class Player():
-    def __init__(self) -> None:
+class Entity:
+    def __init__(self, pos:tuple) -> None:
+        self.pos = pos
         pass
+    
+    def print_pos(self):
+        print(self.pos)
+
+class Player(Entity):
+    def __init__(self) -> None:
+        super().__init__((1,0))
+        self.print_pos()
+    
+
 
 class Enemies():
     def __init__(self) -> None:
